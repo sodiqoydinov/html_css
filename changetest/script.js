d2972feaa996d027f3e6977f1ee9687a83fe7cbb
Sodiq <sodiq7898@gmail.com>
2021-10-18 09:26:56 +0500

    online test
    Loyihaga o'tish :  https://cranky-knuth-23e33b.netlify.app/

---
+++ changetest/script.js
@@ -0,0 +1,24 @@
+function check(){
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+    var q7=document.quiz.question7.value;
+    var q8=document.quiz.question8.value;
+    var q9=document.quiz.question9.value;
+    var q10=document.quiz.question10.value;
+    var q11=document.quiz.question11.value;
+    var q12=document.quiz.question12.value;
+    var q13=document.quiz.question13.value;
+    var q14=document.quiz.question14.value;
+    var q15=document.quiz.question15.value;
+    var q15=document.quiz.question15.value;
+    var q16=document.quiz.question16.value;
+    var result=document.getElementById('result');
+    var quiz = document.getElementById('quiz');
+    quiz.style.display="none"
+    alert(`Siz ${parseInt(q1)+parseInt(q2)+parseInt(q3)+parseInt(q4)+parseInt(q5)+parseInt(q6)+parseInt(q7)+parseInt(q8)+parseInt(q9)+parseInt(q10)+parseInt(q11)+parseInt(q12)+parseInt(q13)+parseInt(q14)+parseInt(q15)+parseInt(q16)} bal yig'dingiz`)
+}
